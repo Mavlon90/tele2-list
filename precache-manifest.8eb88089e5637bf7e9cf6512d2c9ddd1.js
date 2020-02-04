1002fcb3434e515df2cbaa04a149603e7780e298
Mavlon <Mavlon90@users.noreply.github.com>
2020-02-04 12:55:06 +0500

    Updates

---
+++ precache-manifest.8eb88089e5637bf7e9cf6512d2c9ddd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7652f5126402178f4509d7abffcf63ee",
+    "url": "/tele2-list/index.html"
+  },
+  {
+    "revision": "e29006eee59f7b5d5e93",
+    "url": "/tele2-list/static/css/main.9c6a1887.chunk.css"
+  },
+  {
+    "revision": "b1cbdf8dd1bc6ddd0693",
+    "url": "/tele2-list/static/js/2.00a7f061.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tele2-list/static/js/2.00a7f061.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e29006eee59f7b5d5e93",
+    "url": "/tele2-list/static/js/main.e7f6ac9e.chunk.js"
+  },
+  {
+    "revision": "6711efa83d0e444625c1",
+    "url": "/tele2-list/static/js/runtime-main.bb117cc0.js"
+  }
+]);
