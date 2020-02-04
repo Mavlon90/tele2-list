5759f748b7318848bc951d45fe8b815d9f5f86d8
Mavlon <Mavlon90@users.noreply.github.com>
2020-02-04 11:53:54 +0500

    ci & homepage added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import React from 'react';
 import './App.css';
 import TariffsList from './componets/TariffsList/TariffsList';
 import Tariff from './componets/Tariffs/Tariff'
-import Like from './componets/Liker/Like';
-import Test from './componets/Test';
-import Purchase from './componets/Purchase';
 
 function App() {
   return (
     <div className="App">
       <TariffsList />
       <Tariff />
-      {/* <Like />
-      <Test />
-      <Purchase /> */}
+     
     </div>
   );
 }
